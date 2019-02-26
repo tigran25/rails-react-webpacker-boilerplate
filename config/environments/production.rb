7ce755ba79440bc733975611d42e64bd7481a281
tigran25 <tigranvetisyan@gmail.com>
2019-02-26 23:15:02 +0300

    chore: skip email deliveries

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,7 @@ Rails.application.configure do
     :user_name => ENV["SMTP_USERNAME"],
     :password  => ENV["SMTP_PASSWORD"]
   }
+  config.action_mailer.perform_deliveries = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
